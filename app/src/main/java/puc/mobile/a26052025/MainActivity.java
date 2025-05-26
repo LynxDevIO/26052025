1b5ce530ec88e66e92bdf749d407daf569260e5f
Philipe Pedrosa <dev.lynx.io@gmail.com>
2025-05-26 20:46:22 -0300

    commit 2

--- app/src/main/java/puc/mobile/a26052025/MainActivity.java
+++ app/src/main/java/puc/mobile/a26052025/MainActivity.java
@@ -1,6 +1,8 @@
 package puc.mobile.a26052025;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private ListView lista;
+    private ArrayList<User> usuarios = new ArrayList<>();
+    private UserAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +37,30 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        lista = findViewById(R.id.lista_usuarios);
+        adapter = new UserAdapter(this, usuarios);
+        lista.setAdapter(adapter);
+
+        String url = "https://jsonplaceholder.typicode.com/users";
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, response -> {
+            try {
+                for (int i = 0; i < response.length(); i++) {
+                    JSONObject obj = response.getJSONObject(i);
+                    String name = obj.getString("name");
+                    String email = obj.getString("email");
+                    String phone = obj.getString("phone");
+                    usuarios.add(new User(name, email, phone));
+                }
+                adapter.notifyDataSetChanged();
+            } catch (Exception e) {
+                Toast.makeText(this, "Erro ao processar dados", Toast.LENGTH_SHORT).show();
+            }
+        }, error -> {
+            Toast.makeText(this, "Erro de conexão", Toast.LENGTH_SHORT).show();
+        });
     }
 
     private void configurarListeners() {
---
+++ app/src/main/java/puc/mobile/a26052025/UserAdapter.java
@@ -0,0 +1,50 @@
+package puc.mobile.a26052025;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class UserAdapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<User> lista;
+
+    public UserAdapter(Context context, ArrayList<User> lista) {
+        this.context = context;
+        this.lista = lista;
+    }
+
+
+    @Override
+    public int getCount() {
+        return lista.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return lista.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = LayoutInflater.from(context).inflate(android.R.layout.simple_expandable_list_item_2, null);
+        TextView t1 = v.findViewById(R.id.user_name);
+        TextView t2 = v.findViewById(R.id.user_email);
+        TextView t3 = v.findViewById(R.id.user_phone);
+
+        User u = lista.get(position);
+        t1.setText(u.getName());
+        t2.setText(u.getEmail());
+        t2.setText(u.getPhone());
+        return v;
+    }
+}
